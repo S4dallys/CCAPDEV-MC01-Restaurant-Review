5ef2c272286b64a578e12459a81ebbfdfb3180fd
S4dallys <geometrygaming8@gmail.com>
2024-03-11 00:26:31 +0800

    ..

--- local_modules/upload.js
+++
@@ -1,8 +0,0 @@
-const multer = require("multer")
-const upload = multer({ dest: "../public/imgs/uploads/" })
-
-let storate = multer.diskStorage({
-    destination: function(req, file, cb) {
-        db(null, 'uploads')
-    }
-})
