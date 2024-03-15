efbaa98c1508b8be3a745d0c6a984568646cb9fc
Rendell Ngo` <geometrygaming8@gmail.com>
2024-03-15 13:37:34 +0800

    bruh

--- public/js/validateReview.js
+++ public/js/validateReview.js
@@ -1,19 +1,20 @@
-const title = document.getElementById('cr-title')
-const body = document.getElementById('cr-content')
-const button = document.getElementById('cr-post')
-
-
-// disable by default
-button.disabled = true
-
-// check if empty
-title.addEventListener('change', validate) 
-body.addEventListener('change', validate)
-
-function validate() {
-    if(title.value == "" || body.value == ""){
-        button.disabled = true
-    } else {
-        button.disabled = false
-    }
-}
+// const title = document.getElementById('cr-title')
+// const body = document.getElementById('cr-content')
+// const button = document.getElementById('cr-post')
+// 
+// 
+// // disable by default
+// button.disabled = true
+// 
+// // check if empty
+// title.addEventListener('change', validate) 
+// body.addEventListener('change', validate)
+// 
+// function validate() {
+//     if(title.value == "" || body.value == ""){
+//         button.disabled = true
+//     } else {
+//         button.disabled = false
+//     }
+// }
+//
